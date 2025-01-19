d0c079b005c41b0e9a67df4b3a26cfba8ad5b7f5
rizkyhaksono <mrizkyhaksono@gmail.com>
2025-01-19 21:13:18 +0700

    feat(api): add mirror comics on home

--- src/api/controller/home/getHome.ts
+++ src/api/controller/home/getHome.ts
@@ -54,16 +54,39 @@ export default createElysia()
     });
 
     // Get mirror comics
+    const mirrorComics: any = [];
+    $(".recommendations2 .col-xl-2").each((_, element) => {
+      const title = $(element).find(".series-title").text().trim();
+      const image = $(element).find(".thumb-img").attr("src");
+      const link = $(element).find(".series-link").attr("href");
+      const chapters: any = [];
+
+      $(element)
+        .find(".series-chapter-item")
+        .each((_, chapter) => {
+          const chapterTitle = $(chapter).find(".series-badge").text().trim();
+          const chapterTime = $(chapter).find(".series-time").text().trim();
+          chapters.push({ title: chapterTitle, time: chapterTime });
+        });
+
+      mirrorComics.push({
+        title,
+        image,
+        link,
+        chapters,
+      });
+    });
 
     // Get update comics
+    const updateComics = "coming soon";
 
     return {
       status: 200,
       data: {
         "Latest Comics": latestComics,
         "Popular Comis": popularComics,
-        "Mirror": "coming soon",
-        "Update Comics": "coming soon",
+        "Mirror Comics": mirrorComics,
+        "Update Comics": updateComics,
       }
     };
   }, {
