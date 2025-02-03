931344e03bbc83f11bb122c970e41c11bce2ddd7
rizkyhaksono <mrizkyhaksono@gmail.com>
2025-02-03 20:17:38 +0700

    feat(home): new data

--- src/api/controller/home/getHome.ts
+++ src/api/controller/home/getHome.ts
@@ -1,123 +1,74 @@
 import { createElysia } from "@/libs/elysia";
 import * as cheerio from "cheerio";
 
-import ILatestComics from "@/types/latestComics";
-import IMirrorComics from "@/types/mirrorComics";
-import IPopularComics from "@/types/popularComics";
-import IUpdateComicData from "@/types/updateComics";
-
 export default createElysia()
   .get("/", async () => {
     const url = await fetch(`${Bun.env.BASE_URL}`);
     const html = await url.text();
     const $ = cheerio.load(html);
 
-    // Get latest comics
-    const latestComics: ILatestComics[] = [];
-    $(".col-md-8 .col-xl-3").each((_, element) => {
-      const title = $(element).find(".series-title").text().trim();
-      const image = $(element).find(".thumb-img").attr("src");
-      const chapters: any = [];
+    const extractSlug = (url: string | undefined) => {
+      if (!url) return null;
+      const regex = /\/manga\/([^/]+)\/|\/([^/]+-chapter-\d+)\//;
+      const matches = regex.exec(url);
+      return matches ? matches[2] || matches[1] : null;
+    };
 
-      $(element)
-        .find(".series-chapter-item")
-        .each((_, chapter) => {
-          const chapterTitle = $(chapter).find(".series-badge").text().trim();
-          const chapterTime = $(chapter).find(".series-time").text().trim();
-          chapters.push({ title: chapterTitle, time: chapterTime });
-        });
+    const popularComics = $(".listupd.popularslider .bsx").map((_, el) => {
+      const element = $(el);
+      const link = element.find("a").attr("href");
 
-      latestComics.push({
-        title,
-        image,
-        chapters,
-      });
-    });
-
-    // Get popular comics
-    const popularComics: IPopularComics[] = [];
-    $(".d-flex[style*='overflow-x:auto']").find(".col-6").each((_, element) => {
-      const title = $(element).find(".series-title").text().trim();
-      const image = $(element).find(".thumb-img").attr("src");
-      const link = $(element).find(".series-link").attr("href");
-      const chapters: any = [];
-
-      $(element)
-        .find(".series-chapter-item")
-        .each((_, chapter) => {
-          const chapterTitle = $(chapter).find(".series-badge").text().trim();
-          const chapterTime = $(chapter).find(".series-time").text().trim();
-          chapters.push({ title: chapterTitle, time: chapterTime });
-        });
-
-      popularComics.push({
-        title,
-        image,
+      return {
+        title: element.find(".tt").text().trim(),
         link,
-        chapters,
-      });
-    });
-
-    // Get mirror comics
-    const mirrorComics: IMirrorComics[] = [];
-    $(".recommendations2 .col-xl-2").each((_, element) => {
-      const title = $(element).find(".series-title").text().trim();
-      const image = $(element).find(".thumb-img").attr("src");
-      const link = $(element).find(".series-link").attr("href");
-      const chapters: any = [];
-
-      $(element)
-        .find(".series-chapter-item")
-        .each((_, chapter) => {
-          const chapterTitle = $(chapter).find(".series-badge").text().trim();
-          const chapterTime = $(chapter).find(".series-time").text().trim();
-          chapters.push({ title: chapterTitle, time: chapterTime });
-        });
-
-      mirrorComics.push({
-        title,
-        image,
-        link,
-        chapters,
-      });
-    });
-
-    // Get update comics
-    const updateComics: IUpdateComicData[] = [];
-    $(".feed-reaper tbody tr").each((_, element) => {
-      const type = $(element)
-        .find("th span")
-        .text()
-        .trim();
-      const series = {
-        title: $(element).find("td:nth-child(2) a").text().trim(),
-        link: $(element).find("td:nth-child(2) a").attr("href"),
+        slug: extractSlug(link),
+        image: element.find("img").attr("src"),
+        chapter: element.find(".epxs").text().trim(),
+        rating: element.find(".numscore").text().trim(),
       };
-      const chapter = {
-        title: $(element).find("td:nth-child(3) a").text().trim(),
-        link: $(element).find("td:nth-child(3) a").attr("href"),
-      };
-      const date = $(element).find("td:nth-child(4)").text().trim();
+    }).get();
 
-      updateComics.push({
-        type,
-        series,
-        chapter,
-        date,
-      });
-    });
+    const latestComics = $(".listupd .bsx")
+      .map((_, el) => {
+        const element = $(el);
+        const link = element.find(".tt a").attr("href");
+
+        const chapters = element
+          .find(".chfiv li")
+          .map((_, ch) => {
+            const chapterLink = $(ch).find("a").attr("href");
+
+            return {
+              chapter: $(ch).find("a").text().trim(),
+              link: chapterLink,
+              slug: extractSlug(chapterLink),  // Extract the slug here
+              time: $(ch).find(".fivtime").text().trim(),
+            };
+          })
+          .get();
+
+        return chapters.length > 0
+          ? {
+            title: element.find(".tt a").text().trim(),
+            link,
+            slug: extractSlug(link),
+            image: element.find("img").attr("src"),
+            chapters,
+          }
+          : null;
+      })
+      .get()
+      .filter(Boolean);
 
     return {
       status: 200,
       data: {
-        "latestComics": latestComics,
-        "popularComics": popularComics,
-        "mirrorComics": mirrorComics,
-        "updateComics": updateComics,
+        popularComics: popularComics,
+        latestComics: latestComics,
       }
     };
   }, {
     detail: {
       tags: ["Home"]
     }
-  })
+  });
